07351ece6ef33285406335b541c6e33e533670fe
Official Bika <phoetaay@gmail.com>
2026-04-01 00:57:42 +0700

    Add files via upload

--- cmd/bikagame/main.go
+++ cmd/bikagame/main.go
@@ -1,1 +1,31 @@
+package main
 
+import (
+	"context"
+	"log"
+	"os/signal"
+	"syscall"
+
+	appbot "bikagame-go/internal/bot"
+	"bikagame-go/internal/config"
+	"bikagame-go/internal/db"
+)
+
+func main() {
+	cfg := config.Load()
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	dbc, err := db.Connect(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app, err := appbot.New(ctx, cfg, dbc)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.Start(ctx)
+}
