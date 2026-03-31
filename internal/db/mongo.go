22e4db868e2417b467cf8a5e808e09c04fa91794
Official Bika <phoetaay@gmail.com>
2026-04-01 01:05:19 +0700

    Add files via upload

--- internal/db/mongo.go
+++ internal/db/mongo.go
@@ -1,1 +1,42 @@
+package db
 
+import (
+	"context"
+	"time"
+
+	"bikagame-go/internal/config"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+type DB struct {
+	Client       *mongo.Client
+	Database     *mongo.Database
+	Users        *mongo.Collection
+	Groups       *mongo.Collection
+	Config       *mongo.Collection
+	Transactions *mongo.Collection
+	Orders       *mongo.Collection
+}
+
+func Connect(cfg config.Config) (*DB, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(options.Client().ApplyURI(cfg.MongoURI))
+	if err != nil {
+		return nil, err
+	}
+
+	database := client.Database(cfg.DBName)
+
+	return &DB{
+		Client:       client,
+		Database:     database,
+		Users:        database.Collection("users"),
+		Groups:       database.Collection("groups"),
+		Config:       database.Collection("config"),
+		Transactions: database.Collection("transactions"),
+		Orders:       database.Collection("orders"),
+	}, nil
+}
