7c5cec095beff51202a7a5a500326074183c1c52
Official Bika <phoetaay@gmail.com>
2026-04-01 12:08:06 +0700

    Add files via upload

--- internal/models/models.go
+++ internal/models/models.go
@@ -3,30 +3,30 @@ package models
 import "time"
 
 type User struct {
-	UserID             int64      `bson:"userId"`
-	Username           string     `bson:"username,omitempty"`
-	FirstName          string     `bson:"firstName,omitempty"`
-	LastName           string     `bson:"lastName,omitempty"`
-	FullName           string     `bson:"fullName,omitempty"`
-	Balance            int64      `bson:"balance"`
-	IsVIP              bool       `bson:"isVip"`
-	StartedBot         bool       `bson:"startedBot"`
-	StartBonusClaimed  bool       `bson:"startBonusClaimed"`
-	LastDailyClaimDate *string    `bson:"lastDailyClaimDate,omitempty"`
-	CreatedAt          time.Time  `bson:"createdAt"`
-	UpdatedAt          time.Time  `bson:"updatedAt"`
+	UserID             int64   `bson:"userId"`
+	Username           string  `bson:"username,omitempty"`
+	FirstName          string  `bson:"firstName,omitempty"`
+	LastName           string  `bson:"lastName,omitempty"`
+	FullName           string  `bson:"fullName,omitempty"`
+	Balance            int64   `bson:"balance"`
+	IsVIP              bool    `bson:"isVip"`
+	StartedBot         bool    `bson:"startedBot"`
+	StartBonusClaimed  bool    `bson:"startBonusClaimed"`
+	LastDailyClaimDate *string `bson:"lastDailyClaimDate,omitempty"`
+	CreatedAt          time.Time `bson:"createdAt"`
+	UpdatedAt          time.Time `bson:"updatedAt"`
 }
 
 type Group struct {
-	GroupID        int64     `bson:"groupId"`
-	Title          string    `bson:"title"`
-	Username       string    `bson:"username,omitempty"`
-	ApprovalStatus string    `bson:"approvalStatus"`
-	ApprovedBy     *int64    `bson:"approvedBy,omitempty"`
-	BotIsAdmin     bool      `bson:"botIsAdmin"`
-	InviteLink     string    `bson:"inviteLink,omitempty"`
-	CreatedAt      time.Time `bson:"createdAt"`
-	UpdatedAt      time.Time `bson:"updatedAt"`
+	GroupID        int64      `bson:"groupId"`
+	Title          string     `bson:"title"`
+	Username       string     `bson:"username,omitempty"`
+	ApprovalStatus string     `bson:"approvalStatus"`
+	ApprovedBy     *int64     `bson:"approvedBy,omitempty"`
+	BotIsAdmin     bool       `bson:"botIsAdmin"`
+	InviteLink     string     `bson:"inviteLink,omitempty"`
+	CreatedAt      time.Time  `bson:"createdAt"`
+	UpdatedAt      time.Time  `bson:"updatedAt"`
 }
 
 type Treasury struct {
